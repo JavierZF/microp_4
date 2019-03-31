ed918a7adce73ca8600f6efcb700a5c67c8c3f9b
JavierZF <kyoroz98@gmail.com>
2019-03-31 20:06:05 +0200

    Microp 7 done

--- src/pages/libreta-contactos/libreta-contactos.ts
+++ src/pages/libreta-contactos/libreta-contactos.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { NuevoContactoPage }  from   '../nuevo-contacto/nuevo-contacto' ; 
-import  {  Contact  }  from   '../../models/contact.model' ;
-import  {  ContactService  }  from   '../../services/contact.service' ;
+import { VerContactoPage } from '../ver-contacto/ver-contacto'
+import { Contact  }  from   '../../models/contact.model' ;
+import { ContactService  }  from   '../../services/contact.service' ;
+import { Observable  }  from   'rxjs/Observable';
 
 /**
  * Generated class for the LibretaContactosPage page.
@@ -18,19 +20,38 @@ import  {  ContactService  }  from   '../../services/contact.service' ;
 })
 export class LibretaContactosPage {
 
-  contacts :  Contact [] =[];
+  contacts$: Observable<Contact[]>;
+  //contacts :  Contact [] =[];
 
   constructor(public navCtrl: NavController, public navParams: NavParams,  private ContactService :  ContactService) {
   }
 
   ionViewWillEnter (){
-    this.contacts = this.ContactService.getContacts();
+    //this.contacts = this.ContactService.getContacts();
+    this.contacts$ = this.ContactService.getContacts().snapshotChanges().map (
+    /*
+        Estas líneas retornan un array de  objetos con el id del registro y su
+   contenido
+        {
+          "key":"value",
+          contact.name,
+          contact.organization,
+          ...
    }
-
+   */
+    changes => {
+      return changes.map(c=> ({
+        key: c.payload.key, ...c.payload.val()
+      })); 
+    });
+   }
   ionViewDidLoad() {
     console.log('ionViewDidLoad LibretaContactosPage');
   }
   onLoadContactosPage (){
     this.navCtrl.push(NuevoContactoPage);
   }
+  onItemTapped ($event,contact){ 
+     this . navCtrl . push ( VerContactoPage ,  contact );
+  }   
 }
--- src/services/contact.service.ts
+++ src/services/contact.service.ts
@@ -1,17 +1,27 @@
 import  {  Contact  }  from   "../models/contact.model" ;
+import  {  AngularFireDatabase  }  from   "angularfire2/database";
+import  {  Injectable  }  from   "@angular/core" ;
+
+
+@Injectable()
 export class ContactService {  
- private   contacts :  
-     Contact  []= [{ "nombre" : "Andres" , "organizacion" : "UC3M" , "movil" : "666666666" , correo: "andr es@example.com" }];
- constructor (){
+ //private   contacts :  
+// Contact  []= [{ "nombre" : "Andres" , "organizacion" : "UC3M" , "movil" : "666666666" , correo: "andr es@example.com" }];
+  
+
+private contactsRef = this.db.list<Contact>('AgendaFirebase');  
+constructor (private db:AngularFireDatabase){
 }
  addContact ( value :  Contact ){
- this.contacts.push(value); 
+ this.contactsRef.push(value); 
 }
  getContacts(){
- return  this.contacts ;
+ return  this.contactsRef ;
 }
  updateContact ( value :  Contact ){
+     return this.contactsRef.update(value.key,value);
 }
  removeContact ( value :  Contact ){
+    return this.contactsRef.remove(value.key);
 } 
 }
